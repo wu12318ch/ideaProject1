f942729cb003d3a78f2ae517a144e2bd6dcd5c9a
wu12318ch <1325729598@qq.com>
2020-08-11 10:16:19 +0800

    hello1.....

---
+++ Git/src/main/java/cn/kgc/HelloGit.java
@@ -0,0 +1,13 @@
+package cn.kgc;
+
+/**
+ * @author wuchao
+ * @create 2020-08-11 10:03
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+    }
+
+
+}
